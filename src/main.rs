87fb5db04d12355ff8e1bf95ad78339ab50b3f46
Rain <rain@sunshowers.io>
2024-07-26 19:05:34 -0700

    echo servers defeat the purpose

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ struct App {
     #[clap(long, short = 's', global = true)]
     reset_sigpipe: bool,
 
-    /// Number of pings to accept before terminating the connection
+    /// Number of writes to accept before terminating the connection
     #[clap(long, short = 't', global = true)]
-    accept_pings: Option<usize>,
+    accept_writes: Option<usize>,
 
     #[clap(subcommand)]
     cmd: Command,
@@ -35,17 +35,17 @@ impl App {
 
         match self.cmd {
             Command::Tcp => {
-                let addr = spawn_tcp_ping_thread(self.accept_pings)
-                    .context("failed to spawn TCP ping thread")?;
-                eprintln!("TCP ping listening on {}", addr);
-                ping_tcp(&addr).context("failed to ping TCP")?;
+                let addr = spawn_tcp_listener_thread(self.accept_writes)
+                    .context("failed to spawn TCP listener thread")?;
+                eprintln!("TCP listening on {}", addr);
+                loop_write_tcp(&addr).context("failed to write to TCP")?;
             }
             Command::Udp => {
-                let addr = spawn_udp_ping_thread(self.accept_pings)
-                    .context("failed to spawn UDP ping thread")?;
-                eprintln!("UDP ping listening on {}", addr);
+                let addr = spawn_udp_socket_thread(self.accept_writes)
+                    .context("failed to spawn UDP socket thread")?;
+                eprintln!("UDP socket listening on {}", addr);
 
-                ping_udp(&addr).context("failed to ping UDP")?;
+                loop_write_udp(&addr).context("failed to write to UDP")?;
             }
         }
 
@@ -59,7 +59,7 @@ enum Command {
     Udp,
 }
 
-fn spawn_tcp_ping_thread(accept_pings: Option<usize>) -> Result<SocketAddr> {
+fn spawn_tcp_listener_thread(accept_writes: Option<usize>) -> Result<SocketAddr> {
     let listener = TcpListener::bind("127.0.0.1:0").context("failed to bind")?;
     let addr = listener
         .local_addr()
@@ -70,13 +70,11 @@ fn spawn_tcp_ping_thread(accept_pings: Option<usize>) -> Result<SocketAddr> {
             match stream {
                 Ok(mut stream) => {
                     let mut buf = [0; 1024];
-                    for _ in 0..accept_pings.unwrap_or(usize::MAX) {
+                    for current in 0..accept_writes.unwrap_or(usize::MAX) {
                         match stream.read(&mut buf) {
                             Ok(0) => break,
                             Ok(n) => {
-                                if stream.write_all(&buf[..n]).is_err() {
-                                    break;
-                                }
+                                eprintln!("[tcp listener] (current = {current}) read {n} bytes");
                             }
                             Err(_) => break,
                         }
@@ -90,25 +88,21 @@ fn spawn_tcp_ping_thread(accept_pings: Option<usize>) -> Result<SocketAddr> {
     Ok(addr)
 }
 
-fn ping_tcp(addr: &SocketAddr) -> Result<()> {
+fn loop_write_tcp(addr: &SocketAddr) -> Result<()> {
     let mut stream = TcpStream::connect(addr).context("failed to connect")?;
 
     for n in 0.. {
-        eprintln!("ping {}", n);
+        eprintln!("write {}", n);
         stream
             .write_all(b"ping")
             .context("failed to write to socket")?;
 
-        let mut buf = [0; 1024];
-        stream
-            .read(&mut buf)
-            .context("failed to read from socket")?;
         thread::sleep(Duration::from_secs(1));
     }
     Ok(())
 }
 
-fn spawn_udp_ping_thread(accept_pings: Option<usize>) -> Result<SocketAddr> {
+fn spawn_udp_socket_thread(accept_writes: Option<usize>) -> Result<SocketAddr> {
     let listener = UdpSocket::bind("127.0.0.1:0").context("failed to bind")?;
     let addr = listener
         .local_addr()
@@ -116,12 +110,10 @@ fn spawn_udp_ping_thread(accept_pings: Option<usize>) -> Result<SocketAddr> {
 
     thread::spawn(move || {
         let mut buf = [0; 1024];
-        for _ in 0..accept_pings.unwrap_or(usize::MAX) {
+        for current in 0..accept_writes.unwrap_or(usize::MAX) {
             match listener.recv_from(&mut buf) {
                 Ok((n, addr)) => {
-                    if listener.send_to(&buf[..n], addr).is_err() {
-                        break;
-                    }
+                    eprintln!("[udp listener] (current = {current}) read {n} bytes from {addr}");
                 }
                 Err(_) => break,
             }
@@ -131,17 +123,15 @@ fn spawn_udp_ping_thread(accept_pings: Option<usize>) -> Result<SocketAddr> {
     Ok(addr)
 }
 
-fn ping_udp(addr: &SocketAddr) -> Result<()> {
+fn loop_write_udp(addr: &SocketAddr) -> Result<()> {
     // UDP is connectionless, so all we're doing here is creating a socket at some arbitrary
     // address.
     let socket = UdpSocket::bind("127.0.0.1:0").context("failed to connect")?;
 
     for n in 0.. {
-        eprintln!("ping {}", n);
+        eprintln!("write {}", n);
         socket.send_to(b"ping", addr)?;
 
-        let mut buf = [0; 1024];
-        socket.recv_from(&mut buf)?;
         thread::sleep(Duration::from_secs(1));
     }
 
